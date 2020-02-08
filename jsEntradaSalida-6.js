85429eda1e70ddd4633d8eba5007c68c9e67210d
BVSCO <60760431+BVSCO@users.noreply.github.com>
2020-02-07 21:23:41 -0300

    Add files via upload
    5-10

---
+++ jsEntradaSalida-6.js
@@ -0,0 +1,15 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt) y Sumarlos.
+mostrar el resulto por medio de "ALERT"*/
+function sumar()
+{var numerouno
+    var numerodos
+    var resultado
+    numerouno = parseInt(document.getElementById("numeroUno").value);
+    numerodos = parseInt(document.getElementById("numeroDos").value);
+   resultado = numerouno + numerodos;
+alert(resultado);
+    
+
+}
+
---
+++ jsEntradaSalida-9.js
@@ -0,0 +1,13 @@
+/*Debemos lograr tomar el importe por ID ,
+transformarlo a entero (parseInt), luego
+mostrar el importe con un aumento del 10 %
+en el cuadro de texto "RESULTADO".*/
+function mostrarAumento()
+{
+var sueldo
+var aumento
+sueldo = parseInt(document.getElementById("sueldo").value);
+aumento = (sueldo*10/100);
+document.getElementById("resultado").value = aumento + sueldo;
+
+}
